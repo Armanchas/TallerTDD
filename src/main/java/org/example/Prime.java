f78b9478b6dd06e929fcf708a3306ef364c5f2a8
Armin <102205057+Armanchas@users.noreply.github.com>
2024-06-14 11:25:15 -0400

    begin prime test

---
+++ src/main/java/org/example/Prime.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Prime {
+
+    public static boolean isPrime(int i) {
+        return false;
+    }
+}
---
+++ src/test/java/org/example/PrimeTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeTest {
+
+    @Test
+    void isPrime(){
+        boolean result = Prime.isPrime(2);
+    }
+
+}
