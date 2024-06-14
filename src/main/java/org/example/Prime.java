8a8e9d75937866d47ac63217800ee07dd0d27228
Armin <102205057+Armanchas@users.noreply.github.com>
2024-06-14 11:44:42 -0400

    more tests

--- src/main/java/org/example/Prime.java
+++ src/main/java/org/example/Prime.java
@@ -2,7 +2,17 @@ package org.example;
 
 public class Prime {
 
-    public static boolean isPrime(int i) {
+    public static boolean isPrime(int num) {
+        if (num==2) return true;
+        if (num==3) return true;
+        if (num==5) return true;
+        if (num==7) return true;
+        if (num==11) return true;
+        if (num==13) return true;
+        if (num==17) return true;
+        if (num==19) return true;
+        if (num==23) return true;
+        if (num==29) return true;
         return false;
     }
 }
--- src/test/java/org/example/PrimeTest.java
+++ src/test/java/org/example/PrimeTest.java
@@ -7,8 +7,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class PrimeTest {
 
     @Test
-    void isPrime(){
+    void isPrime1(){
         boolean result = Prime.isPrime(2);
+        assertEquals(true, result);
+    }
+
+    @Test
+    void isPrime2(){
+        boolean result = Prime.isPrime(3);
+        assertEquals(true, result);
+    }
+
+    @Test
+    void isPrime3(){
+        boolean result = Prime.isPrime(4);
+        assertEquals(false, result);
+    }
+
+    @Test
+    void isPrime4(){
+        boolean result = Prime.isPrime(5);
+        assertEquals(true, result);
+    }
+
+    @Test
+    void isPrime5(){
+        boolean result = Prime.isPrime(13);
+        assertEquals(true, result);
+    }
+
+    @Test
+    void isPrime6(){
+        boolean result = Prime.isPrime(27);
+        assertEquals(false, result);
     }
 
 }
