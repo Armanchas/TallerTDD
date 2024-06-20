8097bd81d58f9455ac9d15990821ce519827675a
Armin <102205057+Armanchas@users.noreply.github.com>
2024-06-20 18:11:42 -0400

    make constructor

--- src/main/java/org/example/Task.java
+++ src/main/java/org/example/Task.java
@@ -1,6 +1,12 @@
 package org.example;
 
 public class Task {
+    private String title;
+    private String description;
 
+    public Task(String title, String description){
+        this.title = title;
+        this.description = description;
+    }
 
 }
