77b724d1c0d26e85eb2d14533ba9aa1c332b44ff
Armin <102205057+Armanchas@users.noreply.github.com>
2024-06-20 18:06:45 -0400

    partially fill test

--- src/test/java/org/example/TaskTest.java
+++ src/test/java/org/example/TaskTest.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskTest {
+
+    @Test
+    void createTaskTest1(){
+        Task task1 = new Task("get groceries", "buy eggs, bread, milk");
+    }
 
 }
