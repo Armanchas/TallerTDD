488fdb791a095b42412424e89fb75a77daca245d
Armin <102205057+Armanchas@users.noreply.github.com>
2024-06-20 18:50:03 -0400

    rewrite as a list of tasks

--- src/test/java/org/example/TaskTest.java
+++ src/test/java/org/example/TaskTest.java
@@ -1,28 +1,43 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskTest {
 
+    static List<Task> tasks = new ArrayList<>();
+
+    @BeforeAll
+    static void init(){
+        tasks.add(new Task("get groceries", "buy eggs, bread, milk"));
+        tasks.add(new Task("laundry", "put clothes in the washer"));
+        tasks.add(new Task("make lunch", "boil pasta and make sauce, serve on a plate"));
+    }
+
+
     @Test
     void createTaskTest1(){
-        Task task1 = new Task("get groceries", "buy eggs, bread, milk");
-        assertEquals("get groceries", task1.getTitle());
-        assertEquals("buy eggs, bread, milk", task1.getDescription());
+        assertEquals("get groceries", tasks.get(0).getTitle());
+        assertEquals("buy eggs, bread, milk", tasks.get(0).getDescription());
     }
 
     @Test
     void createTaskTest2(){
-        Task task2 = new Task("laundry", "put clothes in the washer");
-        assertEquals("laundry", task2.getTitle());
-        assertEquals("put clothes in the washer", task2.getDescription());
+        assertEquals("laundry", tasks.get(1).getTitle());
+        assertEquals("put clothes in the washer", tasks.get(1).getDescription());
     }
 
     @Test
     void createTaskTest3(){
-        Task task2 = new Task("make lunch", "boil pasta and make sauce, serve on a plate");
-        assertEquals("make lunch", task2.getTitle());
-        assertEquals("boil pasta and make sauce, serve on a plate", task2.getDescription());
+        assertEquals("make lunch", tasks.get(2).getTitle());
+        assertEquals("boil pasta and make sauce, serve on a plate", tasks.get(2).getDescription());
     }
+
+    //------------------------------------------------------------------------------------------------
+
 }
