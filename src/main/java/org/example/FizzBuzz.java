1738a4adc332901b7bacd2eed32c83d0b052b328
ian <i.cicarelli03@ufromail.cl>
2024-06-21 18:57:55 -0400

    FizzBUzz Completo
    Se termina el TDD para FizzBuzz

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -1,15 +1,37 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz {
 
 
     public static boolean esDiviPorTres(int num){
-        int resultado = num%3;
-        if (resultado==0) {
-            return true;
-        }else {
-            return false;
-        }
+        return num%3==0;
+    }
 
+    public static boolean esDiviPorCinco(int num){
+        return num%5==0;
+    }
+
+    public String ImprimeFizzBuzz(int num) {
+        if(esDiviPorTres(num)&&esDiviPorCinco(num)) {
+            return "FizzBuzz";
+        }if (esDiviPorTres(num)) {
+            return "Fizz";
+        }if (esDiviPorCinco(num)){
+            return "Buzz";
+        }else {
+            return String.valueOf(num);
+        }
+    }
+
+
+    public List<String> JuegoFizzBuzz(int inicio, int valorfinal) {
+        List<String> lista = new ArrayList<>();
+        for (int i = inicio; i <=valorfinal ; i++) {
+            lista.add(ImprimeFizzBuzz(i));
+        }
+        return lista;
     }
 }
--- src/test/java/org/example/FizzBuzzTest.java
+++ src/test/java/org/example/FizzBuzzTest.java
@@ -1,7 +1,13 @@
 package org.example;
 
-import org.junit.Before;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,8 +25,13 @@ class FizzBuzzTest {
 
     private FizzBuzz fizzBuzz;
 
+    @BeforeEach
+    public void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
     /**
-     *Primera prueba para verificar la funcion esDiviPorTres para luego crearla
+     *Primera prueba para verificar la funcion esDiviPorTres para luego crearla.
+     * Se crea el metodo en FizzBuzz que retorne true
      */
     @Test
     public void esDivisible() {
@@ -28,7 +39,9 @@ class FizzBuzzTest {
     }
 
     /**
-     * Segunda prueba se implementa metodo para hacer funcionar la prueba
+     * Segunda prueba se implementa metodo para hacer funcionar la prueba.
+     * Se agrega la variable de num para ingresar el numero para luego ser %3 si el resultado es
+     * 0 es divisible y retorna true
      */
     @Test
     public void esDiviPorTres(){
@@ -36,7 +49,8 @@ class FizzBuzzTest {
     }
 
     /**
-     * Tercera prueba probaremos si no es divisible por 4
+     * Tercera prueba probaremos si no es divisible por 4s
+     * Comprobamos que retorne falso si num es = 4
      */
     @Test
     public void esDivisiblePorCuatro(){
@@ -44,6 +58,107 @@ class FizzBuzzTest {
         assertEquals(false,respuesta);
 
     }
+    /**
+     * Cuarta prueba para testear el metodo despues de su refactorizacion.
+     * Se refactoriza y probamos que de verdadero.
+     */
+    @Test
+    public void esDivisibleRefactor(){
+        boolean respuesta = fizzBuzz.esDiviPorTres(17);
+        boolean respuesta2 = fizzBuzz.esDiviPorTres(4);
+        assertEquals(false,respuesta);
+        assertNotEquals(true,respuesta2);
+    }
+    /**
+     * Primera prueba para verificar la funcion esDivisiblePor5 (return true)
+     *
+     */
+    @Test
+    public void esDivisible2(){
+       assertTrue(fizzBuzz.esDiviPorCinco(5),"Es divisible por 5");
+    }
 
+    /**
+     * Se agrega la variable , en caso que num%5=0 retorne true
+     */
+    @Test
+    public void esDivisiblePorCinco(){
+        assertTrue(fizzBuzz.esDiviPorCinco(5),"Es divisible por 5");
+    }
 
+    /**
+     * Comprobaremos si es que retorna false cuando es divide por otro numero que no sea multiplo de 5
+     */
+    @Test
+    public void esDivisiblePorOtro(){
+        boolean respuesta = fizzBuzz.esDiviPorCinco(17);
+        assertEquals(false,respuesta);
+    }
+
+    /**
+     * Refactorizamos el metodo y comprobamos su funcionamiento
+     * @param number es el numero que ingresamos para testear
+     * @param expected es lo que esperaremos de la prueba
+     */
+    @ParameterizedTest
+    @CsvSource({
+            "17, false",
+            "25, true",
+            "15, true",
+            "9, false"
+    })
+    public void esDivisibleRefactor2(int number, boolean expected){
+        boolean resultado = fizzBuzz.esDiviPorCinco(number);
+        assertEquals(expected, resultado);
+    }
+
+    /**
+     * Creamos la prueba para crear el metodo de imprimir FizzBuzz
+     */
+    @Test
+    public void ImprimeFizz(){
+        assertEquals("Fizz",fizzBuzz.ImprimeFizzBuzz(3));
+    }
+    /**
+     * Ahora probaremos si imprime Buzz
+     */
+    @Test
+    public void ImprimeBuzz(){
+        assertEquals("Buzz",fizzBuzz.ImprimeFizzBuzz(5));
+    }
+    /**
+     * Ahora probaremos si imprime FizzBuzz
+     */
+    @Test
+    public void ImprimeFizzBuzz(){
+        assertEquals("FizzBuzz",fizzBuzz.ImprimeFizzBuzz(15));
+    }
+    /**
+     * No es FizzBuzz Ni Fizz NI Buzz caso 1 y 2 retorne nada
+     * @param number es el numero que se ingresa
+     * @param expected es el numero que se espera
+     */
+    @ParameterizedTest
+    @CsvSource({
+            " 2,2",
+            "1,1"
+    })
+    public void NOImprimeFizzBuzz(int number, String expected){
+        String resultado = fizzBuzz.ImprimeFizzBuzz(number);
+        assertEquals(expected, resultado);
+
+    }
+
+    /**
+     * Prueba unitaria para probar todo el funcionamiento
+     *
+     */
+    @Test
+    public void TestPruebaDelJuego(){
+        List<String> resultados = fizzBuzz.JuegoFizzBuzz(1, 15);
+        String[] esperado = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz",
+                "11", "Fizz", "13", "14", "FizzBuzz"};
+        assertArrayEquals(esperado,resultados.toArray());
+
+    }
 }
